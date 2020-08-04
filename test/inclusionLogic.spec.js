10848a5a8c78debd48c6e2e01f14c6255229c326
kghugo <howtostudycantonese@gmail.com>
2020-08-04 23:15:09 +0800

    :white_check_mark: Create test structure

--- test/inclusionLogic.spec.js
+++ test/inclusionLogic.spec.js
@@ -1,1 +1,47 @@
-// TODO
+const postcss = require('postcss')
+const postcssRfsAutopilot = require('../index.js')
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Test shouldBeTransformed()', function () {
+  let css
+
+  beforeEach(function () {
+    css = `p #hello{
+      font-size: 18px;
+      margin: rfs(10rem);
+    }
+
+    body{
+      padding: 5px;
+    }`
+  })
+
+  describe('if a value is already wrapped in rfs()', function () {
+    it('should not be transformed', async function (done) {
+      const options = {
+
+      }
+
+      const result = await postcss([
+        postcssRfsAutopilot(options)
+      ]).process(css, { from: undefined }).then(result => result.css)
+
+      console.log(result)
+      done()
+    })
+  })
+
+  describe('if the unit of a value is not included in includedUnits', function () {
+    it('should not be transformed', function (done) {
+      done()
+    })
+  })
+
+  describe('if the unit of a value is included in excludedUnits', function () {
+    it('should not be transformed', function (done) {
+      done()
+    })
+  })
+})
