ca0ecbad444de752204b868d488c21e05fa734a8
kghugo <howtostudycantonese@gmail.com>
2020-08-04 21:20:33 +0800

    :recycle: Combine similar conditional check in a for...of loop

--- utilities/helper.js
+++ utilities/helper.js
@@ -25,22 +25,18 @@ const shouldBeTransformed = (decl, options) => {
     return false
   }
 
-  if (!isIncluded(decl.prop, options.includedRules) || isIncluded(decl.prop, options.excludedRules)) {
-    console.log(`${decl.prop} excluded`);
-    return false
+  const inclusionRules = [ options.includedRules, options.includedSelectors, options.includedUnits]
+  const exclusionRules = [ options.excludedRules, options.excludedSelectors, options.excludedUnits]
+  const validationValues = [decl.prop, decl.parent.selector, decl.value]
+
+  for(const [index, value] of validationValues.entries() ){
+    if(! isIncluded(value, inclusionRules[index]) || isIncluded(value, exclusionRules[index]) ){
+      console.log(`${decl.parent.selector}{ ${decl.prop}: ${decl.value} } has been excluded`);
+      return false
+    }
   }
 
-  if (!isIncluded(decl.parent.selector, options.includedSelectors) || isIncluded(decl.parent.selector, options.excludedSelectors)) {
-    console.log(`${decl.parent.selector} excluded`);
-    return false
-  }
-
-  if (!isIncluded(decl.value, options.includedUnits) || isIncluded(decl.value, options.excludedUnits)) {
-    console.log(`${decl.value} excluded`);
-    return false
-  }
-
-  console.log(`${decl.parent.selector}{${decl.prop}: ${decl.value}} has been wrapped with rfs()`);
+  console.log(`${decl.parent.selector}{ ${decl.prop}: ${decl.value} } has been wrapped with rfs()`);
   return true
 }
 
