7d6fac14f4037520d0e565186833c0bea393440e
kghugo <howtostudycantonese@gmail.com>
2020-08-05 08:29:04 +0800

    :white_check_mark: Update test for value already wrapped in rfs()

--- test/inclusionLogic.spec.js
+++ test/inclusionLogic.spec.js
@@ -1,56 +1,56 @@
-// const postcss = require('postcss')
-// const postcssRfsAutopilot = require('../index.js')
-//
-// const chai = require('chai')
-// const expect = chai.expect
-//
-// describe('Test shouldBeTransformed()', function () {
-//   let css
-//
-//   beforeEach(function () {
-//     css = `p #hello{
-//       font-size: 18px;
-//       margin: rfs(10rem);
-//     }
-//
-//     body{
-//       padding: 5px;
-//     }`
-//   })
-//
-//   describe('if a value is already wrapped in rfs()', function () {
-//     it('should not be transformed', async function (done) {
-//       const options = {
-//
-//       }
-//
-//       const result = await postcss([
-//         postcssRfsAutopilot(options)
-//       ]).process(css, { from: undefined }).then(result => {
-//         console.log(result)
-//         return result.css
-//       })
-//
-//       console.log(result)
-//       done()
-//     })
-//   })
-//
-//   describe('if the unit of a value is not included in includedUnits', function () {
-//     it('should not be transformed', function (done) {
-//       done()
-//     })
-//   })
-//
-//   describe('if the unit of a value is included in excludedUnits', function () {
-//     it('should not be transformed', function (done) {
-//       done()
-//     })
-//   })
-//
-//   describe('if the unit of a value is included in includedUnits and excludedUnits', function () {
-//     it('should not be transformed', function (done) {
-//       done()
-//     })
-//   })
-// })
+const postcss = require('postcss')
+const postcssRfsAutopilot = require('../index.js')
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Test shouldBeTransformed()', function () {
+  let css
+
+  beforeEach(function () {
+    css = `p #hello{
+      font-size: 18px;
+      margin: rfs(10rem);
+    }
+
+    body{
+      padding: 5px;
+    }`
+  })
+
+  describe('if a value is already wrapped in rfs()', function () {
+    it('should not be transformed', async function () {
+      const options = {
+
+      }
+
+      await postcss([
+        postcssRfsAutopilot(options)
+      ]).process(css, { from: undefined }).then(result => {
+        result.root.walkDecls((decl) => {
+          if (decl.prop === 'margin') {
+            expect(decl.value).to.equal('rfs(10rem)')
+          }
+        })
+      })
+    })
+  })
+
+  describe('if the unit of a value is not included in includedUnits', function () {
+    it('should not be transformed', function (done) {
+      done()
+    })
+  })
+
+  describe('if the unit of a value is included in excludedUnits', function () {
+    it('should not be transformed', function (done) {
+      done()
+    })
+  })
+
+  describe('if the unit of a value is included in includedUnits and excludedUnits', function () {
+    it('should not be transformed', function (done) {
+      done()
+    })
+  })
+})
